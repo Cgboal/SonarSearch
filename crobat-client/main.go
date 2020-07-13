ce5117d940aeaab5be4348efc3349e679204a69b
Calum Boal <cgboal521@gmail.com>
2020-07-13 01:12:47 +0100

    Finished reverse DNS handler

--- crobat-client/main.go
+++ crobat-client/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"encoding/json"
 	"flag"
 	"fmt"
 	crobat "github.com/Cgboal/SonarSearch/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"io"
 	"log"
-	"crypto/tls"
-	"google.golang.org/grpc/credentials"
+	"strings"
 )
 
 type SubdomainResponse []string
@@ -126,9 +127,48 @@ func (c *CrobatClient) GetTlds(domain string, outputType string) {
 
 }
 
+func (c *CrobatClient) ReverseDNS(ipv4 string) {
+	query := &crobat.QueryRequest{
+		Query: ipv4,
+	}
+
+	stream, err := c.client.ReverseDNS(context.Background(), query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		domain, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(domain.Domain)
+	}
+
+}
+
+func (c *CrobatClient) ReverseDNSRange(ipv4 string) {
+	query := &crobat.QueryRequest{
+		Query: ipv4,
+	}
+
+	results, err := c.client.ReverseDNSRange(context.Background(), query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonResults, _ := json.MarshalIndent(*results, "", "    ")
+	fmt.Printf("%s", jsonResults)
+}
+
 func main() {
 	domain_sub := flag.String("s", "", "Get subdomains for this value")
 	domain_tld := flag.String("t", "", "Get tlds for this value")
+	reverse_dns := flag.String("r", "", "Perform reverse lookup on IP address or CIDR range")
 	format := flag.String("f", "plain", "Set output format (json/plain)")
 
 	flag.Parse()
@@ -138,6 +178,12 @@ func main() {
 		client.GetSubdomains(*domain_sub, *format)
 	} else if *domain_tld != "" {
 		client.GetTlds(*domain_tld, *format)
+	} else if *reverse_dns != "" {
+		if !strings.Contains(*reverse_dns, "/") {
+			client.ReverseDNS(*reverse_dns)
+		} else {
+			client.ReverseDNSRange(*reverse_dns)
+		}
 	}
 
 }
